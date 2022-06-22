cae221e126cd161349d5e566cade3de6b39a0d4d
Thomas4390 <vaudescal.t@gmail.com>
2022-06-22 15:34:14 -0400

    [UPDATE] minor changes

--- Week_1/lin_reg_class.py
+++ Week_1/lin_reg_class.py
@@ -1,6 +1,6 @@
 import numpy as np 
 
-class Linreg:
+class LinReg:
 
     # default setup: lr=0.01 and epoch=800
 
@@ -8,7 +8,7 @@ class Linreg:
         self.lr = lr
         self.epochs = epochs
         self.weights = None
-        self.biais = None
+        self.bias = None
 
     # Training function: fit
     def fit(self, X, y):
@@ -18,6 +18,47 @@ class Linreg:
         # n --> number of features 
         m, n = X.shape
 
-        # Initializing 
+        # Initializing a column vector of zeros of size : (number of features, 1)
+        # and bias as 0. 
         self.weights = np.zeros((n, 1))
-        self.biais = 0
+        self.bias = 0
+
+        # Reshaping y as (m, 1) in case your dataset initialized as 
+        # (m, ) which can cause problems
+        y = y.reshape(m, 1)
+
+        # empty list to store losses so we can plot them later
+        # against epochs
+
+        losses = []
+
+        for epoch in range(self.epochs):
+
+            # Calculating prediction y_hat or h(x)
+            y_hat = np.dot(X, self.weights) + self.bias
+
+            # Calculating loss
+            loss = np.mean((y_hat - y)**2)
+
+            # Appending loss to losses
+            losses.append(loss)
+
+            # Calculating derivatives of parameters (weights and bias)
+            dw = (1/m) * np.dot(X.T, (y_hat - y))
+            db = (1/m) * np.sum((y_hat - y))
+
+            # Updating the parameters: parameter := parameter - lr*derivative
+            # of loss/cost w.r.t parameter
+
+            self.weights -= self.lr * dw
+            self.bias = self.lr * db
+
+        # Returning the parameter so we can look at them later
+        return self.weights, self.bias, losses
+
+    # Predicting(calculating y_hat with our updated weights) for the 
+    # testing/validation     
+    def predict(self, X):
+        return np.dot(X, self.weights) + self.bias
+
+
